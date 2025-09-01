3578261c666cc469265acfe36021755791ab4ac9
Occy55284 <grahammark55284@gmail.com>
2025-09-01 13:10:21 +0100

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+// Fill these in with YOUR values from Supabase → Settings → API
+window.SUPABASE_URL = "https://xxqykjiuowunwdiskhnk.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4cXlraml1b3d1bndkaXNraG5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MjY0MTcsImV4cCI6MjA3MjMwMjQxN30.jdmL2nU07BwoSHxaGavnymhBpxA2HYnPOzNerR_Qa6I";
